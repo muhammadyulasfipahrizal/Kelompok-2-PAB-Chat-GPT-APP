b038b53a609262dea04649e0a4e004ee52db794d
muhammad yulasfi pahrizal <muhammadpahrizal790@gmail.com>
2023-10-21 14:22:33 +0800

    .

--- app/src/main/java/com/app/chatgpt/API.java
+++ app/src/main/java/com/app/chatgpt/API.java
@@ -2,5 +2,5 @@ package com.app.chatgpt;
 
 public class API {
     public static String API_URL = "https://api.openai.com/v1/completions";
-    public static String API = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    public static String API = "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";
 }
